7da9505815d5b577fa6d44c8f56a09bab31f0646
inanygt <inanygt@hotmail.com>
2023-09-22 18:29:54 +0200

    added styling intropage

--- src/app/all-products/all-products.component.ts
+++ src/app/all-products/all-products.component.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CategoryService } from '../category.service';
 
 @Component({
   selector: 'app-all-products',
   templateUrl: './all-products.component.html',
   styleUrls: ['./all-products.component.css'],
 })
-export class AllProductsComponent {}
+export class AllProductsComponent implements OnInit {
+  categories: any;
+  constructor(private categoryService: CategoryService) {}
+  ngOnInit(): void {
+    this.categoryService.getCategories().subscribe((data) => {
+      console.log(data);
+      this.categories = data;
+    });
+  }
+}
