6742e6aecfdce7365ee60e8e6f323c160c886af8
inanygt <inanygt@hotmail.com>
2023-09-26 10:11:12 +0200

    seperated modal structure profile in footer

--- src/app/components/MODALS/profile/profile.component.ts
+++ src/app/components/MODALS/profile/profile.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.css']
+  styleUrls: ['./profile.component.css'],
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  navigateToVoorwaarden() {
+    this.router.navigate(['/voorwaarden']);
+    this.activeModal.close();
+  }
 
+  constructor(public activeModal: NgbActiveModal, private router: Router) {}
+
+  ngOnInit(): void {}
 }
