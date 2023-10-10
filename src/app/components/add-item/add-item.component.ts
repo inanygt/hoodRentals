bcec7020a7f7c58b3d215235b848852a76ee9f84
inanygt <inanygt@hotmail.com>
2023-10-10 13:50:33 +0200

    post req working now fixed bug

--- src/app/components/add-item/add-item.component.ts
+++ src/app/components/add-item/add-item.component.ts
@@ -8,7 +8,13 @@ import {
 } from '@ng-bootstrap/ng-bootstrap';
 import { LoginComponent } from 'src/app/login/login.component';
 import { AuthenticationService } from 'src/app/services/authentication.service';
-import { FormControl, FormGroup } from '@angular/forms';
+import {
+  Form,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 
 @Component({
@@ -17,69 +23,45 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./add-item.component.css'],
 })
 export class AddItemComponent implements OnInit {
-  constructor(
-    public itemService: ItemService,
-    public activeModal: NgbActiveModal,
-    private http: HttpClient
-  ) {}
-
   private url = 'http://localhost:8000/api';
-
   addItemForm: FormGroup;
-
-  ngOnInit(): void {
-    this.addItemForm = new FormGroup({
-      title: new FormControl(''),
-      description: new FormControl(''),
-      city: new FormControl('Genk'),
-      region: new FormControl('limburg'),
-      price_per_day: new FormControl(''),
-      price_per_extra_day: new FormControl(''),
-      category_id: new FormControl(''),
-      subcategory_id: new FormControl(''),
-      subsubcategory_id: new FormControl(''),
-      user_id: new FormControl(''),
-      image: new FormControl(''),
-      borrowed: new FormControl(''),
-      borrowable: new FormControl(''),
-      visible: new FormControl(''),
-    });
-  }
 
   selected: string = 'huur';
   title: string = '';
-
   selectedToggleBtn: string = '';
 
-  onFontStyleChange(event: any) {
-    this.selectedToggleBtn = event.value;
+  constructor(
+    public itemService: ItemService,
+    public activeModal: NgbActiveModal,
+    private http: HttpClient,
+    private fb: FormBuilder
+  ) {
+    this.addItemForm = this.fb.group({
+      title: ['', Validators.required],
+      description: ['', Validators.required],
+      visible: ['', Validators.required],
+      borrowable: ['', Validators.required],
+      price_per_day: ['', Validators.required],
+      price_per_extra_day: ['', Validators.required],
+    });
   }
 
-  onSubmit() {
-    const formData = {
-      title: 'test',
-      city: 'genk',
-      region: 'limburg',
-      price_per_day: 5,
-      price_per_extra_day: 5,
-      category_id: 1,
-      subcategory_id: 1,
-      subsubcategory_id: 1,
-      user_id: 1,
-      image: 'test',
-      borrowed: 10,
-      description: 'testing',
-      borrowable: true,
-      visible: true,
-    };
+  ngOnInit(): void {}
+
+  onSubmit(formData: any) {
+    console.log(formData);
 
     this.http.post(this.url + '/items', formData).subscribe(
       (res) => {
-        console.log('Post created successfully', res);
+        console.log('Post Created Succesfully', res);
       },
       (error) => {
-        console.error('Error creating post', error);
+        console.log('Error creating post', error);
       }
     );
+  }
+
+  onFontStyleChange(event: any) {
+    this.selectedToggleBtn = event.value;
   }
 }
